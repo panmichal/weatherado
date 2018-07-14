457fd047092eab038ed93a81655d317d8cf8287b
Michal Strzelczyk <mich.strzelczyk@gmail.com>
2018-07-14 21:25:03 +0200

    Working mnesia cache

--- lib/weatherado/reports/current_conditions.ex
+++ lib/weatherado/reports/current_conditions.ex
@@ -3,6 +3,7 @@ defmodule Weatherado.Reports.CurrentConditions do
 alias Weatherado.DataProviders.Accuweather.Provider
 alias Weatherado.DataProviders.Accuweather.Parser
 alias Weatherado.Cache.Mnesia.Weather, as: Cache
+alias Weatherado.Structs.Temperature
 
 use Amnesia
 use Weatherado.Cache.Mnesia.Weather
@@ -10,30 +11,34 @@ require Logger
 
     def get(location_id) do
         case get_from_cache(location_id) do
-            nil -> location_id
+            [] -> location_id
                     |> Provider.current_conditions
                     |> Parser.parse
-                    |> add_to_cache
+                    |> add_to_cache(location_id)
+                    |> IO.inspect
             result -> {:ok, result}
         end            
     end
 
-    defp get_from_cache(location_id) do
-        Amnesia.transaction do
-          selection = Cache.CurrentConditions.where location_id == location_id,
-          select: [temperature, unit]
+    defp get_from_cache(id) do
+        cached = Amnesia.transaction do
+          selection = Cache.CurrentConditions.where(location_id == id)
 
-          selection |> Amnesia.Selection.values |> Enum.each &IO.inspect(&1)
+          selection |> Amnesia.Selection.values
         end
-        nil
+
+        case cached do
+            [] -> []
+            [conditions] -> %Weatherado.Structs.CurrentConditions{temperature: %Temperature{unit: Map.get(conditions, :unit), value: Map.get(conditions, :temperature)}}
+        end
     end
 
-    defp add_to_cache({:ok, %Weatherado.Structs.CurrentConditions{} = conditions}) do
+    defp add_to_cache({:ok, %Weatherado.Structs.CurrentConditions{} = conditions}, location_id) do
         Amnesia.transaction do
             cache_content = %Cache.CurrentConditions{
                 temperature: conditions.temperature.value, 
                 unit: conditions.temperature.unit, 
-                location_id: "13"
+                location_id: location_id
             }
             
             Logger.info "Writing to cache #{inspect cache_content}"
@@ -42,7 +47,7 @@ require Logger
         end
         {:ok, conditions}
     end
-    defp add_to_cache({status, result}), do: {status, result}
+    defp add_to_cache({status, result}, _location_id), do: {status, result}
     
     
 end
