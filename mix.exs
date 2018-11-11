5311efcfb1ed4b1efcc2ce673c724aae2f3c3fdf
Michal Strzelczyk <mich.strzelczyk@gmail.com>
2018-11-11 15:00:55 +0100

    Change Amnesia version and add exquisite dep to fix 1.7.* compatibility issues

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Weatherado.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 1.0"},
-      {:amnesia, "~> 0.2.7"},
+      {:amnesia, github: "meh/amnesia", branch: "master", override: true},
+      {:exquisite, github: "meh/exquisite", branch: "master", override: true},
       {:distillery, "~> 2.0"}
     ]
   end
